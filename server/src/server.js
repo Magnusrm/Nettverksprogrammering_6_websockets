ab31db0818c121e35b0f940a7900ee41077b2e2b
Ilia <ilia.rad.saadat@hotmail.com>
2019-04-03 11:36:06 +0200

    Backend completed

--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,54 @@
 // @flow
 
+
 import express from 'express';
 import path from 'path';
 import reload from 'reload';
 import fs from 'fs';
-import { Students } from './models.js';
+
 
 type Request = express$Request;
 type Response = express$Response;
+let WebSocket = require('ws');
 
-const public_path = path.join(__dirname, '/../../client/public');
+const public_path = path.join(__dirname, '/../../client/public'); //to download client
 
 let app = express();
 
 app.use(express.static(public_path));
 app.use(express.json()); // For parsing application/json
+
+
+
+let websocket = new WebSocket.Server({port: 3001});
+
+websocket.on('connction',(connection) => {
+  console.log('A connection is now open');
+
+  connection.on('message', (message) => {
+
+    console.log('Client has sent a message: ' + message);
+    websocket.clients.map(e => {
+      if(e.readyState === WebSocket.OPEN){
+        e.send(JSON.stringify('Yo! whazzuuup client. You sent this message ' + message));
+      }
+    });
+  });
+
+  connection.on('close',() => {
+    console.log('Closed the connection');
+  });
+
+  connection.on('error', (error) => {
+    console.log('got error: ' + error.message);
+  })
+
+});
+
+
+
+
+
 
 // Hot reload application when not in production environment
 if (process.env.NODE_ENV !== 'production') {
